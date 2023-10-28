ab64ac332c6a92040cc1dcf5d47f36c7be0ed3ab
Guy Borthwick <guyborthwick@yahoo.com>
2023-10-28 08:59:41 +0100

    Air bnb challenge simple - started

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create!(
+  name: 'Dark and smelly flat',
+  address: '10 Oakley Street, London SW5 1DT',
+  description: 'Classic London Studio',
+  price_per_night: 25,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Gorgeous riverside flat',
+  address: '34 Finborough Road, SW10 9EG',
+  description: 'Open Plan',
+  price_per_night: 205,
+  number_of_guests: 5
+)
+
+Flat.create!(
+  name: 'Gorgeous house',
+  address: '64 Finborough Road, SW10 9EG',
+  description: 'Open Plan',
+  price_per_night: 205,
+  number_of_guests: 5
+)
